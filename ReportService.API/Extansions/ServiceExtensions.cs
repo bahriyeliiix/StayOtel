de03a971e0331464cb396d03116ce1071a8ab006
Furkan Aydın <furkan.aydin@ist.aero>
2024-11-28 16:20:52 +0300

    add extensions in reportService

---
+++ ReportService.API/Extansions/ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelService.API.Extansions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+           
+        }
+    }
+}
