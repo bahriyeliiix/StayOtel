d021d2edae07878b7b24f231f954a5542bf8c6e4
Furkan Aydın <furkan.aydin@ist.aero>
2024-11-28 16:19:33 +0300

    add dbContext in hotelService

---
+++ HotelService.Infrastructure/Data/HotelServiceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelService.Infrastructure.Data
+{
+    public class HotelServiceDbContext : DbContext
+    {
+        public HotelServiceDbContext(DbContextOptions<HotelServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
