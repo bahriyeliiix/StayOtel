45713056728980e499eff1519009774f3e850796
Furkan Aydın <furkan.aydin@ist.aero>
2024-11-28 16:19:55 +0300

    add dbContext in ReportService

---
+++ ReportService.Infrastructure/Data/ReportServiceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReportService.Infrastructure.Data
+{
+    public class ReportServiceDbContext : DbContext
+    {
+        public ReportServiceDbContext(DbContextOptions<ReportServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
