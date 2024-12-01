0b061e88319863e0ee791787243f72d9ac672428
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 21:50:14 +0300

    DI

--- HotelService/Extansions/ServiceExtensions.cs
+++ HotelService/Extansions/ServiceExtensions.cs
@@ -1,10 +1,26 @@
-﻿namespace HotelService.API.Extansions
+﻿using System.Reflection;
+using System.Runtime.Loader;
+using HotelService.Application.Features.Hotels.Handlers;
+using HotelService.Application.Mapping;
+using HotelService.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelService.API.Extansions
 {
     public static class ServiceExtensions
     {
         public static void AddCustomServices(this IServiceCollection services)
         {
-           
+            services.AddAutoMapper(typeof(HotelServiceMappingProfile));
+
+
+
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+
+
+
+            services.AddScoped<IHotelRepository,HotelRepository>();
         }
     }
 }
