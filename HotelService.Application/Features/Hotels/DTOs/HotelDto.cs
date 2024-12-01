091192367ff4978a336fef6c1aab3fb316d93619
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 21:51:25 +0300

    create Dto

---
+++ HotelService.Application/Features/Hotels/DTOs/HotelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelService.Application.Features.Hotels.DTOs;
+
+public class HotelDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
