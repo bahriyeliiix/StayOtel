118b2e423d990941a9ce3989293abda6e524edb1
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 21:49:39 +0300

    create hotelrepository

---
+++ HotelService.Infrastructure/Repositories/HotelRepository.cs
@@ -0,0 +1,59 @@
+﻿using HotelService.Domain.Entities;
+using HotelService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelService.Infrastructure.Repositories
+{
+    public interface IHotelRepository
+    {
+        Task<Hotel> GetByIdAsync(Guid id);
+        Task<List<Hotel>> GetAllAsync();
+        Task AddAsync(Hotel hotel);
+        Task UpdateAsync(Hotel hotel);
+        Task DeleteAsync(Hotel hotel);
+    }
+
+    public class HotelRepository : IHotelRepository
+    {
+        private readonly HotelServiceDbContext _context;
+
+        public HotelRepository(HotelServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Hotel> GetByIdAsync(Guid id)
+        {
+            return await _context.Hotels
+                                 .Include(h => h.Contacts)
+                                 .Include(h => h.Managers)
+                                 .FirstOrDefaultAsync(h => h.Id == id);
+        }
+
+        public async Task<List<Hotel>> GetAllAsync()
+        {
+            return await _context.Hotels
+                                 .Include(h => h.Contacts)
+                                 .Include(h => h.Managers)
+                                 .ToListAsync();
+        }
+
+        public async Task AddAsync(Hotel hotel)
+        {
+            await _context.Hotels.AddAsync(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Hotel hotel)
+        {
+            _context.Hotels.Update(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Hotel hotel)
+        {
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
