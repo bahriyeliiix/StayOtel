b9a6aebdede42553fce7ff0aa75215f5e05bba9e
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 20:16:34 +0300

    create entities and database migration

--- HotelService.Infrastructure/Data/HotelServiceDbContext.cs
+++ HotelService.Infrastructure/Data/HotelServiceDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelService.Infrastructure.Data
 {
@@ -9,11 +10,66 @@ namespace HotelService.Infrastructure.Data
         {
         }
 
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelManager> HotelManagers { get; set; }
+        public DbSet<HotelContact> HotelContacts { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<HotelManager>()
+                .HasOne(hm => hm.Hotel)
+                .WithMany(h => h.Managers)
+                .HasForeignKey(hm => hm.HotelId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<HotelContact>()
+                .HasOne(hc => hc.Hotel)
+                .WithMany(h => h.Contacts)
+                .HasForeignKey(hc => hc.HotelId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<Hotel>()
+                .Property(h => h.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()"); 
+
+            modelBuilder.Entity<HotelManager>()
+                .Property(hm => hm.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            modelBuilder.Entity<HotelContact>()
+                .Property(hc => hc.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            modelBuilder.Entity<Hotel>()
+                .Property(h => h.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<HotelContact>()
+                .Property(hc => hc.ContactType)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<HotelContact>()
+                .Property(hc => hc.ContactDetail)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            modelBuilder.Entity<HotelManager>()
+                .Property(hm => hm.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<HotelManager>()
+                .Property(hm => hm.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<HotelManager>()
+                .Property(hm => hm.Position)
+                .HasMaxLength(50);
         }
     }
 }
---
+++ HotelService.Infrastructure/Migrations/20241201170758_ConfigureHotelServiceEntities.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureHotelServiceEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelContacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContactType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ContactDetail = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotelContacts_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelManagers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelManagers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotelManagers_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelContacts_HotelId",
+                table: "HotelContacts",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelManagers_HotelId",
+                table: "HotelManagers",
+                column: "HotelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelContacts");
+
+            migrationBuilder.DropTable(
+                name: "HotelManagers");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
