e97bca8da5b79125bf4e48cbcb43826faffa9665
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 21:49:24 +0300

    create apiResponseModel

---
+++ Shared/Wrappers/ApiResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Shared.Wrappers
+{
+    public class ApiResponse<T>
+    {
+        public ApiResponse()
+        {
+        }
+
+        public ApiResponse(T data, string message = null, bool success = true, int statusCode = 200)
+        {
+            Data = data;
+            Message = message;
+            Success = success;
+            StatusCode = statusCode;
+        }
+
+        public T Data { get; set; } 
+        public string Message { get; set; } 
+        public bool Success { get; set; } 
+        public int StatusCode { get; set; }
+    }
+}
