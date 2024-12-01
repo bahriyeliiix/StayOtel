50214695b62f4fc8b81e10d02ec6092ddc53c2c8
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-01 21:49:06 +0300

    add mapping

---
+++ HotelService.Application/Mappings/HotelMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HotelService.Application.Features.Hotels.DTOs;
+using HotelService.Domain.Entities;
+using HotelService.Application.Features.Hotels.Commands;
+
+namespace HotelService.Application.Mapping
+{
+    public class HotelServiceMappingProfile : Profile
+    {
+        public HotelServiceMappingProfile()
+        {
+            // Hotel Mappings
+            CreateMap<Hotel, HotelDto>();
+            CreateMap<HotelDto, Hotel>();
+
+            CreateMap<Hotel, CreateHotelCommand>();
+            CreateMap<CreateHotelCommand, Hotel>();
+
+            //// HotelManager Mappings
+            //CreateMap<HotelManager, HotelManagerDto>();
+            //CreateMap<HotelManagerDto, HotelManager>();
+            //CreateMap<HotelManager, CreateHotelManagerCommand>();
+
+            //// HotelContact Mappings
+            //CreateMap<HotelContact, HotelContactDto>();
+            //CreateMap<HotelContactDto, HotelContact>();
+            //CreateMap<HotelContact, CreateHotelContactCommand>();
+        }
+    }
+}
