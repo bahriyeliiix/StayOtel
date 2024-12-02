f26e584953bfb53a4a3a9b52a57f76141be60d14
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 14:06:13 +0300

    edit manager entity prop

---
+++ HotelService.Infrastructure/Migrations/20241202103439_EditHotelManagerEntitiy.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditHotelManagerEntitiy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Position",
+                table: "HotelManagers",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "HotelManagers",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "HotelManagers");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "HotelManagers",
+                newName: "Position");
+        }
+    }
+}
