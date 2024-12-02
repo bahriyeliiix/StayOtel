dacf76929b74af119fe532822dae2570d9129fcd
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 15:34:26 +0300

    add mapping profile

---
+++ ReportService.Application/Mappings/ReportProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReportService.Application.Features.DTOs;
+
+namespace ReportService.Application.Mappings
+{
+    public class ReportProfile : Profile
+    {
+        public ReportProfile()
+        {
+            CreateMap<ReportData, ReportDto>().ReverseMap();
+            CreateMap<ReportDto, ReportData>().ReverseMap();
+        }
+    }
+}
