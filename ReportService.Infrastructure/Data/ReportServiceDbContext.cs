1616095626e2b1536475dc00f5c9f7cbddc03d92
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 15:44:27 +0300

    add dbcontext

--- ReportService.Infrastructure/Data/ReportServiceDbContext.cs
+++ ReportService.Infrastructure/Data/ReportServiceDbContext.cs
@@ -4,14 +4,19 @@ namespace ReportService.Infrastructure.Data
 {
     public class ReportServiceDbContext : DbContext
     {
-        public ReportServiceDbContext(DbContextOptions<ReportServiceDbContext> options)
-            : base(options)
-        {
-        }
+        public DbSet<ReportData> Reports { get; set; }
+
+        public ReportServiceDbContext(DbContextOptions<ReportServiceDbContext> options) : base(options) { }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ReportData>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Status).HasMaxLength(50);
+                entity.Property(e => e.Location).IsRequired();
+            });
         }
     }
 }
