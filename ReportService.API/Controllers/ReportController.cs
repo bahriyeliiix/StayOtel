2a99d9e81d77303d384e52d88c5824b33a459c5e
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 16:58:52 +0300

    reports service api

---
+++ ReportService.API/Controllers/ReportController.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ReportService.Application.Features.Commands;
+using ReportService.Application.Features.DTOs;
+using ReportService.Application.Features.Queries;
+using ReportService.Application.Interfaces;
+using Shared.Messaging;
+using Shared.Wrappers;
+
+namespace ReportService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IReportProducer _reportProducer;
+
+        public ReportsController(IMediator mediator, IReportProducer reportProducer)
+        {
+            _mediator = mediator;
+            _reportProducer = reportProducer;
+        }
+
+
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateHotel([FromBody] CreateReportCommand command)
+        {
+            if (command == null)
+                return BadRequest(new ApiResponse<string>(null, "report data is null", false, 400));
+
+            var result = await _mediator.Send(command);
+
+            var createReportMessage = new CreateReportMessage
+            {
+                ReportId = result,
+                RequestedAt = DateTime.UtcNow,
+                Location = command.Location
+            };
+            await _reportProducer.PublishReportRequestAsync(createReportMessage);
+
+            return Ok(new ApiResponse<Guid>(result, "successfully"));
+        }
+
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetHotel(Guid id)
+        {
+            var query = new GetReportByIdQuery();
+            var report = await _mediator.Send(query);
+
+            if (report == null)
+                return NotFound(new ApiResponse<object>(null, "report not found", false, 404));
+
+            return Ok(new ApiResponse<ReportDto>(report, "report fetched successfully"));
+        }
+
+      
+
+
+
+
+    }
+}
---
+++ ReportService.Infrastructure/Repositories/ReportRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReportService.Infrastructure.Data;
+
+namespace ReportService.Infrastructure.Repositories
+{
+    public interface IReportRepository
+    {
+        Task<ReportData> AddAsync(ReportData report);
+        Task<ReportData?> GetByIdAsync(Guid id);
+        Task<List<ReportData>> GetAllAsync();
+    }
+    public class ReportRepository : IReportRepository
+    {
+        private readonly ReportServiceDbContext _context;
+
+        public ReportRepository(ReportServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ReportData> AddAsync(ReportData report)
+        {
+            await _context.Reports.AddAsync(report);
+            _context.SaveChanges();
+
+            return report;
+        }
+
+        public async Task<ReportData?> GetByIdAsync(Guid id)
+        {
+            return await _context.Reports
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<List<ReportData>> GetAllAsync()
+        {
+            return await _context.Reports.ToListAsync();
+        }
+
+
+    }
+}
