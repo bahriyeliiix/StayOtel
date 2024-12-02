f510cefe7ba7871cd3343a03c0baae30a0418b03
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 15:45:17 +0300

    create Report Entity

---
+++ ReportService.Domain/Entities/ReportData.cs
@@ -0,0 +1,11 @@
+﻿using ReportService.Domain.Enums;
+using Shared.Entities;
+
+public class ReportData : BaseEntity
+{
+    public DateTime RequestedAt { get; set; }
+    public ReportStatus Status { get; set; }
+    public string Location { get; set; }
+    public int HotelCount { get; set; }
+    public int PhoneCount { get; set; }
+}
