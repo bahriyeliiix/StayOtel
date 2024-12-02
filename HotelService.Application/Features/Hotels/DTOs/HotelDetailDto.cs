117a529c0fe464e6dfa6f64d1ee521614c2d10a3
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 14:11:51 +0300

    hoteldetail endponit create

---
+++ HotelService.Application/Features/Hotels/DTOs/HotelDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelService.Application.Features.HotelManagers.DTOs;
+using HotelService.Application.Features.Hotels.Queries;
+
+namespace HotelService.Application.Features.Hotels.DTOs
+{
+    public class HotelDetailDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<HotelManagerDto> Managers { get; set; }
+        public List<HotelContactDto> Contacts { get; set; }
+
+    }
+}
--- HotelService.Application/Features/Hotels/Handlers/GetHotelByIdQueryHandler.cs
+++ HotelService.Application/Features/Hotels/Handlers/GetHotelByIdQueryHandler.cs
@@ -3,35 +3,38 @@ using Microsoft.EntityFrameworkCore;
 using HotelService.Application.Features.Hotels.DTOs;
 using HotelService.Infrastructure.Data;
 using HotelService.Application.Features.Hotels.Queries.GetHotelById;
+using HotelService.Domain.Entities;
+using AutoMapper;
+using HotelService.Infrastructure.Repositories;
+using Shared.Exceptions;
 
 namespace HotelService.Application.Features.Hotels.Handlers
 {
-    public class GetHotelByIdQueryHandler : IRequestHandler<GetHotelByIdQuery, HotelDto>
+    public class GetHotelByIdQueryHandler : IRequestHandler<GetHotelByIdQuery, HotelDetailDto>
     {
-        private readonly HotelServiceDbContext _context;
+        private readonly IHotelRepository _hotelRepository;
 
-        public GetHotelByIdQueryHandler(HotelServiceDbContext context)
+        private readonly IMapper _mapper;
+
+
+        public GetHotelByIdQueryHandler( IMapper mapper, IHotelRepository hotelRepository)
         {
-            _context = context;
+            _mapper = mapper;
+            _hotelRepository = hotelRepository;
         }
 
-        public async Task<HotelDto> Handle(GetHotelByIdQuery request, CancellationToken cancellationToken)
+        public async Task<HotelDetailDto> Handle(GetHotelByIdQuery request, CancellationToken cancellationToken)
         {
-            var hotel = await _context.Hotels
-                .AsNoTracking()
-                .FirstOrDefaultAsync(h => h.Id == request.Id, cancellationToken);
+            var hotel = await _hotelRepository.GetByIdAsync(request.Id);
 
             if (hotel == null)
-                return null;
-
-            return new HotelDto
             {
-                Id = hotel.Id,
-                Name = hotel.Name,
-                Address = hotel.Address,
-                City = hotel.City,
-                Country = hotel.Country
-            };
+                throw new NotFoundException("Hotel not found");
+            }
+
+
+            return _mapper.Map<HotelDetailDto>(hotel);
+         
         }
     }
 }
