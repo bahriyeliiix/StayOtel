1355f219a960d0ed9bb4cf3c774f18f8517568d9
Furkan Aydın <furkan.aydin@ist.aero>
2024-12-02 15:45:00 +0300

    create mesagge model

---
+++ Shared/Messaging/CreateReportMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Messaging
+{
+    public class CreateReportMessage
+    {
+        public Guid ReportId { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public string Location { get; set; }
+    }
+}
